9348742b896c83a86c843140cd6fbe6f0eed0759
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-06-26 18:22:05 -0500

    created 3-print_functions.c and updated main.h

---
+++ 3-print_functions.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+void print_character(va_list args)
+
+void print_string(va_list args)
+
+void print_percent(va_list args)
+
+void print_decimal(va_list args)
+
+void print_integer(va_list args)
+
--- main.h
+++ main.h
@@ -10,6 +10,10 @@ struct specifier
 int _printf (const char *format, ...);
 int (*get_spec_func(char *s))(va_list args);
 int _strcmp(char *s1, char *s2);
-
+void print_character(va_list args);
+void print_string(va_list args);
+void print_percent(va_list args);
+void print_decimal(va_list args);
+void print_integer(va_list args);
 #endif
 
