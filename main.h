486b6df4d1c202b6603128e258d28ba13f1ea477
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-06-26 17:46:58 -0500

    create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef main_h
+#define main_h
+
+struct specifier
+{
+	char *spec;
+	void (*f)(va_list);
+};
+
+int _printf (const char *format, ...);
+int (*get_spec_func(char *s))(va_list args);
+
+#endif
