d4de90f892b35da9d4388918ad45e66bae7802b5
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-26 21:04:51 -0500

    made the get_spec_func file and added all of the necessary code for this step in the process, made it betty compliant as well

--- 2-get_spec_func.c
+++ 2-get_spec_func.c
@@ -2,13 +2,31 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- *
- *
- *
- *
- *
+ * get_spec_func - pointer function to the format specifiers functions
+ * Description - this function is a pointer function that takes
+ * one argument and passes it to the matching format specifiers
+ * function as stated in the struct
+ * @s: this is the format specifier that is identified after the
+ * percent sign and passed to this function
+ * Return: Always 0 (success)
 */
 int (*get_spec_func(char *s))(va_list args)
 {
-	
+	spec_t specs[] = {
+		{"c", print_character},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 3)
+	{
+		if ((_strcmp(specs[i].spec, s)) == 0)
+		{
+			return (specs[i].f);
+		}
+		
+	}
+	return (NULL);
 }
