15bcd315fa8616e86c106064f0b31a6422bf419c
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-26 20:54:09 -0500

    I added a typedef to the struct we have in the main.h file and satisfied betty

--- main.h
+++ main.h
@@ -1,11 +1,17 @@
 #ifndef main_h
 #define main_h
 
-struct specifier
+/**
+ * struct specifier - struct specifier
+ * 
+ * @spec: the format specifier
+ * @f: the function associated
+*/
+typedef struct specifier
 {
 	char *spec;
 	void (*f)(va_list);
-};
+} spec_t;
 
 int _printf (const char *format, ...);
 int (*get_spec_func(char *s))(va_list args);
