38d6106e5644b256db9b86bd524d75fe26bf5d27
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-06-27 05:12:52 -0500

    wrote out the code for our print string function in the print_functions file, also added betty comment formatting for eat function in the file.

--- 3-print_functions.c
+++ 3-print_functions.c
@@ -1,14 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_character - 
+ * Description - 
+ * Return: Always 0 (success)
+*/
 void print_character(va_list args)
+{
 
+}
+/**
+ * print_string - prints a string of characters
+ * Description - initializes a pointer to the passed argument
+ * and prints it out
+ * Return: Always 0 (success)
+*/
 void print_string(va_list args)
+{
+	int c = 0;
+	char *str = va_arg(args, char *);
 
+	while (str[c] != '\0')
+	{
+		putchar(str[c]);
+		c++;
+	}
+}
+/**
+ * print_percent - 
+ * Description - 
+ * Return: Always 0 (success)
+*/
 void print_percent(va_list args)
+{
 
+}
+/**
+ * print_decimal - 
+ * Description - 
+ * Return: Always 0 (success)
+*/
 void print_decimal(va_list args)
+{
 
+}
+/**
+ * print_integer - 
+ * Description - 
+ * Return: Always 0 (success)
+*/
 void print_integer(va_list args)
+{
 
+}
