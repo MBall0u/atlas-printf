bacfc3c374e47ae1309eb3619c9e7d5143471cbc
Harrison  Gearhart <harrisongearhart@Harrisons-MacBook-Air.local>
2024-06-29 20:33:15 -0500

    3-print_functions/added print_int&dec funcs

--- 3-print_functions.c
+++ 3-print_functions.c
@@ -40,24 +40,42 @@ void print_percent(va_list args __attribute__ ((unused)))
 	putchar('%');	
 }
 /**
- * print_decimal - 
- * Description - 
- * Return: Always 0 (success)
+ * print_decimal - initializes a pointer to an array of ints and a decimal point, and prints them.
+ * Description - prints a decimal number
+ * Return: Void
 */
-/**
- * void print_decimal(va_list args)
-{
 
+void print_decimal(va_list args)
+{
+	int n = 0;
+	char *dec = va_arg(args, char *)
+	
+	while (dec[n] != '\0' && dec[n] != '.')
+	{
+		putchar(dec[i];)
+		n++;
+	
+		if (dec[n] == '.')
+		{
+			putchar('.')
+		}
+	}	
 }
-*/
+
 /**
  * print_integer - 
  * Description - 
  * Return: Always 0 (success)
 */
-/**
+
 void print_integer(va_list args)
 {
-
+	int i = 0;
+	char *number = va_arg(va_list, char *)
+	
+	while (number[i] != '\0')
+	{
+		putchar(number[i]);
+		i++;
+	}
 }
-*/
